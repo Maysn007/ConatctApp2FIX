fab14d5dc72ca70acd956eb4c07720329cde8626
Yannick Kreuter <y.kreuterle@sbg.at>
2025-03-11 19:19:18 +0100

    erneuter Zwischenstand 11.03.2025

--- ContactApp2.ContactManager/pages/AddPage.xaml.cs
+++ ContactApp2.ContactManager/pages/AddPage.xaml.cs
@@ -1,9 +1,12 @@
+using ContactApp2.Core.ViewModels;
+
 namespace ContactApp2.ContactManager.pages;
 
 public partial class AddPage : ContentPage
 {
-	public AddPage()
+	public AddPage(AddViewModel viewModel)
 	{
 		InitializeComponent();
-	}
+		this.BindingContext = viewModel;
+    }
 }
--- ContactApp2.Core/ViewModels/AddViewModel.cs
+++ ContactApp2.Core/ViewModels/AddViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace ContactApp2.Core.ViewModels;
 
-public class AddViewModel : ObservableObject
+public partial class AddViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string _firstname = string.Empty;
 
+    [ObservableProperty]
+    private string _lastname = string.Empty;
+
+    [ObservableProperty]
+    private string _phone = string.Empty;
+
+    [RelayCommand]
+    void Save()
+    {
+
+    }
 }
